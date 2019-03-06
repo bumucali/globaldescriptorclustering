36e34bbb269549705c165a907550b5932ef47f01
bumucali <berkayumucalilar@gmail.com>
2019-03-06 18:44:56 +0100

    initial commit

--- descriptor.py
+++ descriptor.py
@@ -1,4 +1,6 @@
 import pandas
 
+
 descriptor_values = pandas.read_csv('/home/berkay/Desktop/EndDescriptors/esf/DescriptorValuesESF.csv')
+descriptor_values = descriptor_values.drop(descriptor_values.columns[640], axis = 1)
 print(descriptor_values)
