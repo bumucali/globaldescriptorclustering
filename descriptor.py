0c9cfbde6c19716155c4ce0031ae39fe6bdad33b
bumucali <berkayumucalilar@gmail.com>
2019-03-07 14:30:29 +0100

    k-means algorithm(2 variations)

--- descriptor.py
+++ descriptor.py
@@ -1,6 +1,48 @@
 import pandas
+import pylab as pl
 
+from sklearn.cluster import KMeans
 
-descriptor_values = pandas.read_csv('/home/berkay/Desktop/EndDescriptors/esf/DescriptorValuesESF.csv')
-descriptor_values = descriptor_values.drop(descriptor_values.columns[640], axis = 1)
-print(descriptor_values)
+if __name__ == '__main__':
+
+    label_cluster = []
+    score_cluster = []
+    descriptor_values = pandas.read_csv('/home/berkay/Desktop/EndDescriptors/esf/DescriptorValuesESF.csv')
+    descriptor_values = descriptor_values.drop(descriptor_values.columns[640], axis=1)
+    cluster_range = range(50, 200)
+
+    for cluster_size in cluster_range:
+        k_means = KMeans(n_clusters=cluster_size, init='random')
+        print(k_means.fit(descriptor_values).labels_)
+        score_cluster.append(k_means.fit(descriptor_values).score(descriptor_values))
+
+    pl.plot(cluster_range, score_cluster)
+
+    pl.xlabel('Number of Clusters')
+
+    pl.ylabel('Score')
+
+    pl.title('Elbow Curve')
+
+    pl.show()
+
+    """
+    Nc = range(35, 50)
+
+    k_means = [KMeans(n_clusters=i, init='random') for i in Nc]
+
+    score = [k_means[i].fit(descriptor_values).score(descriptor_values) for i in range(len(k_means))]
+
+    pl.plot(Nc, score)
+
+    pl.xlabel('Number of Clusters')
+
+    pl.ylabel('Score')
+
+    pl.title('Elbow Curve')
+
+    pl.show()
+
+    """
+
+print('END')
